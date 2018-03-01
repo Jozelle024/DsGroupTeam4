630531c457f6b4fa103606b297ee1acdef0697c9
Alessandro Corrado <ale.corrado91@gmail.com>
2018-03-01 17:57:15 +0100

    Inizio sviluppo con sass

--- gulpfile.js
+++ gulpfile.js
@@ -1,1 +1,13 @@
 // jshint ignore: start
+const gulp = require('gulp');
+const sass = require('gulp-sass');
+
+gulp.task('sass', function(){
+    return gulp.src('./sass/*.scss')
+        .pipe(sass().on('error', sass.logError))
+        .pipe(gulp.dest('./css'));
+});
+
+gulp.task('sass:watch', function(){
+    gulp.watch('./sass/*.scss',['sass']);
+})
